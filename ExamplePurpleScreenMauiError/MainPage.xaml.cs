0fb5ead1103e5f0bdd2ba80420a7a9d3a9708d76
max <max@MS-DESK-01>
2024-01-08 09:47:09 +0000

    Add project files.

---
+++ ExamplePurpleScreenMauiError/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamplePurpleScreenMauiError
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExamplePurpleScreenMauiError/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamplePurpleScreenMauiError
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
